06fd9be1fe9c482e9d9dee7d31ecc65d7cb70b9e
Barnogul <103810800+barnogulwebdeveloper@users.noreply.github.com>
2022-04-16 11:34:39 +0300

    2-project iteration
    CRUD ammalari

---
+++ post/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Article, Comment
+
+# from post.models import Person
+
+# admin.site.register(Person)
+admin.site.register(Article)
+admin.site.register(Comment)
+
+
+# Register your models here.
